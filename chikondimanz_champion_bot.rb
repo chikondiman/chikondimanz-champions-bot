cddead6c1212222e2dbd5cb66b912b37c0036b37
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-03-12 14:30:56 -0600

    add do loop

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -15,13 +15,18 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
     else puts "ah, carry on then"
     
     end
-puts "do you need encouragement? (yes/no)"
-needs_encouragement = gets.chomp
-
-   while needs_encouragement.lowcase == "yes"
-puts "if you can believe it you're half way there"
-puts "do  you still need in encouragement?"
-needs_encouragement = gets.chomp
-break if needs_encouragement.lowcase == "no"
-   end
     
+    puts "do you need encouragement? (yes/no)"
+    needs_encouragement = gets.chomp
+    
+    while needs_encouragement.downcase == "yes"
+    puts "if you can believe it you're half way there"
+    puts "do  you still need in encouragement?"
+    needs_encouragement = gets.chomp
+    break if needs_encouragement.downcase == "no"
+       end
+        
+10.times do
+puts "you can do it"
+puts "YOU CAN DO IT"
+end
