9c08e32de99eabe1fa651d0aa624f01b0c7a26b8
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-04-23 10:38:10 -0500

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -30,7 +30,7 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
     puts "And where were you born?"
     location = gets.chomp
     puts "Finally, what is your special talent?"
-    talent = gets.chomp
+    talent = gets.chomprdfrgw
     puts "Why yes you are the chosen champion #{name} born #{birthday} in #{location} with monumental strength in #{talent}. Pleasure to have your aquaintance. May blessing & favor be upon you and your children's children."
     
     elsif is_champown.downcase == "maybe"
@@ -83,7 +83,7 @@ words.each do |word|
   if word != redact
     print word + " "
   else
-    print "REDACTED foo"
+    print "REDACTED again foo"
   end
 end
 
