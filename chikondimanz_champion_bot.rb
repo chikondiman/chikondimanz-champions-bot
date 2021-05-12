4868b8f22803cf98e18d7143ee84a45c676dfa79
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-05-12 18:16:21 -0500

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -29,7 +29,7 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
     birthday = gets.chomp
     puts "And where were you born?"
     location = gets.chomp
-    puts "Finally, what is your special talent?"
+    puts "Lastly, what is your special talent?"
     talent = gets.chomprdfrgw
     puts "Why yes you are the chosen champion #{name} born #{birthday} in #{location} with monumental strength in #{talent}. Pleasure to have your aquaintance. May blessing & favor be upon you and your children's children."
     
