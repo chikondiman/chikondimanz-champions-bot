f4248c56084a0e0a25c184bd0e1a7a333e318197
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-05-13 14:08:50 -0500

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -34,7 +34,7 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
     puts "Why yes you are the chosen champion #{name} born #{birthday} in #{location} with monumental strength in #{talent}. Pleasure to have your aquaintance. May blessing & favor be upon you and your children's children."
     
     elsif is_champown.downcase == "maybe"
-      puts "well make up your mind"
+      puts "well make up your mind, indecision will be your demise"
     else 
       puts "well, carry on then"
     
