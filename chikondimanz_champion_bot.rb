8228c33e9d20ab5bc7bf6a128c028943ee56c001
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-05-18 07:56:29 -0500

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -43,7 +43,7 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
 if name == "Jayla Janae Corley-Crusoe" 
     puts "so you are the leader champion to restore peace throughout the world huh"
 else
-  puts "so you are the champion that has come"
+  puts "so you are the champion that has come."
 end
 
     puts "tell me, do you need encouragement? (yes/no)"
