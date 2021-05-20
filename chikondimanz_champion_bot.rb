595cfe50a16ef80da735452ab00774b995ddc1b9
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-05-20 09:33:21 -0500

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -27,7 +27,7 @@ puts "I am searching for champions, do you believe that you are a champion?(yes/
     name = gets.chomp
     puts "I must also know, what is your day of birth?"
     birthday = gets.chomp
-    puts "And where were you born, young champion?"
+    puts "And where were you born, champion?"
     location = gets.chomp
     puts "Lastly, what is your special talent?"
     talent = gets.chomprdfrgw
