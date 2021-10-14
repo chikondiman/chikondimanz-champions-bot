0ecb5d929b610fc032f6322bc8c464c7f24811e8
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-14 19:18:20 -0400

    Update chikondimanz_champion_bot.rb

--- chikondimanz_champion_bot.rb
+++ chikondimanz_champion_bot.rb
@@ -22,7 +22,9 @@ jayla = Champion.new("Jayla", 012197,"Music", "Houston")
 juni = Champion.new("Juni", 111120,"Dog Commands", "Houston")
 
 
+def start()
 puts "I am searching for champions. Do you believe that you're a champion? (yes/no)"
+end
 
 def makeAchampion(is_champion, name, birthday, talent, location)
 
